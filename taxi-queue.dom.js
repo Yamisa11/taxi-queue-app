9cfa31464ee309ed9cde9449c79a29c31b4f6820
Siyabonga <c.ya@live.com>
2023-05-26 11:35:45 +0200

    first commit

--- taxi-queue.dom.js
+++ taxi-queue.dom.js
@@ -1,6 +1,10 @@
 // write your DOM code here.
-
-
+var passengerCount = document.querySelector(".passenger_queue_count")
+var taxiCount = document.querySelector(".taxi_queue_count")
+var passengerJoin = document.querySelector(".join_queue")
+var passengerLeave = document.querySelector(".leave_queue")
+var taxiJoin = document.querySelector(".join_taxi_queue")
+var taxiDepart = document.querySelector(".depart")
 // DOM element references
 
 // create Factory Function instance
@@ -8,4 +12,35 @@
 const taxiQueue = TaxiQueue();
 
 // DOM events
+var numPassengers = JSON.parse(localStorage.getItem("passengers"))
+passengerCount.innerHTML = numPassengers;
+var numTaxis = JSON.parse(localStorage.getItem("taxis"))
+taxiCount.innerHTML = numTaxis;
 
+
+passengerJoin.addEventListener('click', function(){
+     taxiQueue.joinQueue()
+    localStorage["passengers"] = JSON.stringify(taxiQueue.queueLength());
+    passengerCount.innerHTML = taxiQueue.queueLength()
+})
+
+passengerLeave.addEventListener('click', function(){
+    taxiQueue.leaveQueue()
+    localStorage["passengers"] = JSON.stringify(taxiQueue.queueLength())
+    passengerCount.innerHTML = taxiQueue.queueLength()
+})
+
+taxiJoin.addEventListener('click', function(){
+    taxiQueue.joinTaxiQueue()
+    localStorage["taxis"] = JSON.stringify(taxiQueue.taxiQueueLength())
+    taxiCount.innerHTML = taxiQueue.taxiQueueLength()
+})
+
+taxiDepart.addEventListener('click', function(){
+    taxiQueue.taxiDepart()
+    localStorage["taxis"] = JSON.stringify(taxiQueue.taxiQueueLength())
+    taxiCount.innerHTML = taxiQueue.taxiQueueLength()
+    localStorage["passengers"] = JSON.stringify(taxiQueue.queueLength());
+    passengerCount.innerHTML = taxiQueue.queueLength()
+
+})
--- taxi-queue.js
+++ taxi-queue.js
@@ -1,29 +1,35 @@
 function TaxiQueue() {
-
+var persons = 0;
+var taxiNumber = 0;
 
 	function joinQueue() {
-
+	 persons++;
 	}
 
 	function leaveQueue() {
-
+	 if (persons>0) {
+		persons--
+	 }
 	}
 
 	function joinTaxiQueue() {
-
+		taxiNumber++
 	}
-
-	function queueLength() {
-
-	}
-
-	function taxiQueueLength() {
-
-	}
-
+	
 	function taxiDepart(){
-
+		if (taxiNumber>1 && persons>=12) {
+			taxiNumber--
+			persons -= 12
+		}
 	}
+	function taxiQueueLength() {
+		return taxiNumber;
+		
+	}
+	function queueLength() {
+		return persons;
+	}
+
 
 	return {
 		joinQueue,
